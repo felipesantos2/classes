9bb129907bf551bb0454068d6a9da462a2f6c028
Felipe <81455073+felipesantos2@users.noreply.github.com>
2025-12-05 11:45:19 +0000

    feat: add classes for test

---
+++ classes/patterns/builder/Singleton.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class Singleton {
+
+    private static ?Singleton $_instance = null;
+
+    private function __construct() {}
+
+    public static function instance(): static
+    {
+        if(is_null(static::$_instance)) {
+            $_instance = new static();
+        } 
+
+        return static::$_instance;
+    }
+
+}
+
+
+$obj = Singleton::instance();
+
---
+++ classes/patterns/singleton/Singleton.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Singleton {
+
+    private static ?Singleton $_instance = null;
+
+    private function __construct() {}
+
+    public static function instance(): static
+    {
+        if(is_null(static::$_instance)) {
+            $_instance = new static();
+        } 
+
+        return static::$_instance;
+    }
+
+}
+
+
+$obj = Singleton::instance();
