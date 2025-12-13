f223ed3dd7b75e48cdabda8f310997de1bc6b987
felipesantos2 <santospinheiro627@gmail.com>
2025-12-13 17:02:56 -0300

    add teste

--- classes/patterns/builder/Singleton.php
+++ classes/patterns/builder/Singleton.php
@@ -1,23 +1,22 @@
 <?php
 
 
-class Singleton {
-
+class Singleton
+{
     private static ?Singleton $_instance = null;
 
     private function __construct() {}
 
     public static function instance(): static
     {
-        if(is_null(static::$_instance)) {
+        if (is_null(static::$_instance)) {
             $_instance = new static();
-        } 
+        }
 
         return static::$_instance;
     }
 
+    public static function teste(): void {}
 }
 
-
-$obj = Singleton::instance();
-
+Singleton::instance();
